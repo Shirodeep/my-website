deb50d91ddea4d38cd8bc574823bd472b3c1fe52
shirodeep <shirodeepdahal0987@gmail.com>
2022-07-23 11:17:59 +0545

    Modified home a little and contact is also updated

--- src/partials/mainComponent.js
+++ src/partials/mainComponent.js
@@ -7,14 +7,12 @@ function MyHome(){
            <div  className="infos">
                 <h1 className="heading">Home</h1>
                 <p className="my-words"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.
-                </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus doloribus quia inventore unde eum dolore mollitia ex eaque modi, dolor quos deserunt reprehenderit ut nobis laboriosam expedita illum doloremque.</p>
+                    I got influenced by many individuals that I watched n youtube.
+                    From that day I am interested in technical fields and want to do everything I can in these feilds.
+                    I am always in front of laptop I got after high school and I found interesting devices, tools, technology.
+                    Nowadays I like to do small projects to achieve my dream of being software engineer. 
+               </p>
+                <p>Passionate in coding and adventurer.</p>
            </div>
             <img src="./images/logo.jpg" className="image-for-home"/>
         </div>      
@@ -27,23 +25,28 @@ function Projects(){
         <div className="project-text" id="projects">
             < Project 
             img="./images/pathfindingalgorithm.png" 
-            name="Path Finding Algorithm Using Python"
+            name="Path Finding Algorithm "
             github="https://github.com/Shirodeep/Use-Of-Path-Finding-Algorithm-python/tree/master"
+            projectStatus = "Project Completed"
+            blablabla = "True"
             />
             < Project 
             img="./images/tetrisgame.png" 
             name="Tetris Game Using Python"
             github="https://github.com/Shirodeep/Tetris-using-python.git"
+            projectStatus = "Project Completed"
             />
             < Project 
-            img="./images/macncoffee.jpeg" 
-            name="Code of this page"
+            img="./images/reactHtmlJsCss.webp" 
+            name="Webpage using React"
             github="https://github.com/Shirodeep/my-website.git"
+            projectStatus = "Project Incomplete"
             />
              < Project 
             img="./images/nodeExpressMongo.jpg" 
             name="Blog site"
             github="https://github.com/Shirodeep/Blogsite.git"
+            projectStatus = "Project Completed"
             />
             
         </div>
@@ -62,16 +65,24 @@ function About(){
 function Contact(){
     return(
         <div className="contact" id="contact">
-            <div>
+            <div className="email-holder">
                 <h1>To contact me.</h1>
-                <input type="email"/>
-                <textarea></textarea>
-                <input type="submit" value="Submit"/>
+                <div className="contact-message">
+                   <div className="first-box">
+                        <label className="label1" for="email">Email:</label>
+                        <input type="email"id="email" className="inputs1"/>
+                    </div>
+                    <div className="second-box">
+                        <label  className="label2"for="message">Message:</label>
+                        <textarea id="message" className="inputs2" rows="4" cols="29"></textarea>
+                    </div>
+                    <div className="submit-button"><input type="submit" value="Submit" className="inputs"/></div>
+                </div>
             </div>
             <ul className="contact-list">
                 <li><a href="https://github.com/shirodeep"><i className=" fa fa-github"></i></a></li>
-                <li><a href="https://linkedin.com/carlousMarkus"><i className="fa fa-linkedin"></i></a></li>
-                <li><a href="https://twitter.com/carlous"><i className="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/shirodeep-dahal-9877a7210/"><i className="fa fa-linkedin"></i></a></li>
+                <li><a href="https://twitter.com/carlousMarkus"><i className="fa fa-twitter"></i></a></li>
             </ul>
         </div>
     )
--- src/partials/projects.js
+++ src/partials/projects.js
@@ -7,8 +7,8 @@ export default function Project(values){
             <img className="image-for-projects" src={values.img} height="200px" width="230px" alt="mac andd coffee"/>
             <p>{values.name}</p>
             <div className="rating">   
-                <a href={values.github}><i className="fa fa-github">click for code</i></a>             
-                <hr/><p>Good project</p>
+                <a className="project-github-link" href={values.github}><i className="fa fa-github">&nbsp; &nbsp;click for code</i></a>             
+                <hr className="hr"/><p>{values.projectStatus}</p>
             </div>    
         </div>
     )
